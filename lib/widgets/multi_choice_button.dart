f152201cdeb48275ec15232af9be0308b6b3c947
TheGreedyGoat <alexander_schertl@teilnehmer.cdemy.de>
2026-06-17 12:06:27 +0200

    ...

---
+++ lib/widgets/multi_choice_button.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class MultiChoiceButton extends StatefulWidget {
+  const MultiChoiceButton({
+    required this.onChanged,
+    required this.items,
+    this.selectedBackgroundColor,
+    this.unselectedBackgroundColor,
+    this.unselectedForegroundColor,
+    this.selectedForegroundColor,
+    super.key,
+  });
+  final List<MultiChoiceButtonItem> items;
+  final Color? unselectedBackgroundColor;
+  final Color? selectedBackgroundColor;
+  final Color? unselectedForegroundColor;
+  final Color? selectedForegroundColor;
+
+  final void Function(int value) onChanged;
+
+  @override
+  State<MultiChoiceButton> createState() => _MultiChoiceButtonState();
+}
+
+class _MultiChoiceButtonState extends State<MultiChoiceButton> {
+  List<MultiChoiceButtonItem> get items => widget.items;
+  int selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
+    widget.onChanged(items[selectedIndex].value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: items
+          .map(
+            (item) => OutlinedButton(
+              onPressed: () {
+                setState(() {
+                  selectedIndex = items.indexOf(item);
+                });
+              },
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.horizontal(
+                    start: items.first == item ? Radius.circular(100) : null,
+                    end: items.last == item ? Radius.circular(100) : null,
+                  ),
+                ),
+                backgroundColor: items.indexOf(item) == selectedIndex
+                    ? widget.selectedBackgroundColor ?? Colors.black
+                    : widget.unselectedBackgroundColor ?? Colors.white,
+                foregroundColor: items.indexOf(item) == selectedIndex
+                    ? widget.selectedForegroundColor ?? Colors.white
+                    : widget.selectedForegroundColor ?? Colors.black,
+              ),
+              child: item.child,
+            ),
+          )
+          .toList(),
+    );
+  }
+}
+
+class MultiChoiceButtonItem {
+  MultiChoiceButtonItem({this.child, required this.value});
+  final Widget? child;
+  final int value;
+}

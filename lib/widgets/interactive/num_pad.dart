6190af5cfaa33e2ca5f09c274aae4d088f8dd9ea
TheGreedyGoat <alexander_schertl@teilnehmer.cdemy.de>
2026-06-24 12:03:22 +0200

    ...

---
+++ lib/widgets/interactive/num_pad.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:my_utils/widgets/interactive/uni_button.dart';
+
+class NumPad extends StatefulWidget {
+  const NumPad({this.preset = ButtonPreset.elevated, super.key});
+  final ButtonPreset preset;
+  @override
+  State<NumPad> createState() => _NumPadState();
+}
+
+class _NumPadState extends State<NumPad> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: [
+        for (int i = 1; i <= 9; i++)
+          UniButton(
+            buttonPreset: widget.preset,
+            onPressed: () {
+              print(i);
+            },
+            child: Text(i.toString()),
+          ),
+      ],
+    );
+  }
+}

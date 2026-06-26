80600e0c8414db7544e1023e4304ed3be3610093
TheGreedyGoat <alexander_schertl@teilnehmer.cdemy.de>
2026-06-26 09:50:54 +0200

    double formatting utility

---
+++ lib/utility/formatters.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/services.dart';
+
+class Formatters {
+  static RegExp doubleInputRegExp(int fractionDigits) =>
+      RegExp(r'^\d+[.,]?\d{0,#}|^$'.replaceAll('#', fractionDigits.toString()));
+
+  static TextInputFormatter doubleFormatter(int fractions) {
+    return FilteringTextInputFormatter.allow(doubleInputRegExp(fractions));
+  }
+
+  static String displayDouble(
+    double d, {
+    int fractionDigits = 2,
+    String prefix = '',
+    String suffix = '',
+  }) => '$prefix ${d.toStringAsFixed(2)} $suffix';
+}

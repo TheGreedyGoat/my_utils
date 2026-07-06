3bbd01fe07669933c3b00622e24eb729ab0fe4c3
TheGreedyGoat <alexander_schertl@teilnehmer.cdemy.de>
2026-07-06 15:35:35 +0200

    added double int conversion

--- lib/utility/formatters.dart
+++ lib/utility/formatters.dart
@@ -1,11 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/services.dart';
 
+/// povides a couple of functions for specific text input formats aswell as converting data from and to different representations
 class Formatters {
   const Formatters._();
   static RegExp doubleInputRegExp(int fractionDigits) =>
       RegExp(r'^\d+[.,]?\d{0,#}|^$'.replaceAll('#', fractionDigits.toString()));
 
-  static TextInputFormatter doubleFormatter(int fractions) {
+  static TextInputFormatter fixedDigitsDoubleFormatter(int fractions) {
     return FilteringTextInputFormatter.allow(doubleInputRegExp(fractions));
   }
 
@@ -15,4 +18,14 @@ class Formatters {
     String prefix = '',
     String suffix = '',
   }) => '$prefix ${d.toStringAsFixed(2)} $suffix';
+
+  /// converts a double to an integer representation.
+  static int doubleToInt(double value, int fractionDigits) {
+    return (value * pow(10, fractionDigits)).round();
+  }
+
+  /// converts an integer representation back to a double
+  static double intToDouble(int value, int fractionDigits) {
+    return value.toDouble() / pow(10, fractionDigits);
+  }
 }
